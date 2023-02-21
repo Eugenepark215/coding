bc7e5a47aecf19ab6cdc3867f783ed7e939561f9
Eugene Park <epark215@gmail.com>
2023-02-21 05:31:59 +0000

    inital commit commonKeyValuePairs

---
+++ unionTypes/commonKeyValuePairs/index.ts
@@ -0,0 +1,21 @@
+// type Like = {
+//   username: string;
+//   displayName: string;
+// };
+
+// type Share = {
+//   username: string;
+// };
+
+// function getFriendNameFromEvent(event) {
+//   return event.displayName || event.username;
+// }
+
+// const newEvent = {
+//   username: 'vkrauss',
+//   displayName: 'Veronica Krauss',
+// };
+
+// const friendName = getFriendNameFromEvent(newEvent);
+
+// console.log(`You have an update from ${friendName}.`);
