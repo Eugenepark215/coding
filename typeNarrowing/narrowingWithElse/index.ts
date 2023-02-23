6eed45b168dadcbc8aa9a37d25539e2c8bc69617
Eugene Park <epark215@gmail.com>
2023-02-23 05:11:17 +0000

    Initial commit narrowingWithElse

---
+++ typeNarrowing/narrowingWithElse/index.ts
@@ -0,0 +1,25 @@
+// type Pasta = {
+//   menuName: string;
+//   boil: () => string;
+// }
+
+// type Meat = {
+//   menuName: string;
+//   panFry: () => string;
+// }
+
+// const fettuccine = {
+//   menuName: 'Fettuccine',
+//   boil: () => 'Heat water to 212 degrees'
+// };
+
+// const steak = {
+//   menuName: 'New York Strip Steak',
+//   panFry: () => 'Heat oil to 350 degrees'
+// };
+
+// function prepareEntree(entree: Pasta | Meat) {
+//   return entree.boil();
+// }
+
+// console.log(prepareEntree(fettuccine));
