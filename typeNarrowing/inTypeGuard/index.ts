c257754310a00a2d70f020e4fbf02c98029e409b
Eugene Park <epark215@gmail.com>
2023-02-23 03:51:26 +0000

    Initial commit inTypeGuards

---
+++ typeNarrowing/inTypeGuard/index.ts
@@ -0,0 +1,25 @@
+// type Cat = {
+//   name: string;
+//   run: () => string;
+// }
+
+// type Fish = {
+//   name: string;
+//   swim: () => string;
+// }
+
+// const siameseCat = {
+//   name: 'Proxie',
+//   run: () => 'pitter pat'
+// };
+
+// const bettaFish = {
+//   name: 'Neptune',
+//   swim: () => 'bubble blub'
+// };
+
+// function move(pet: Cat | Fish) {
+//   return pet.run();
+// }
+
+// console.log(move(siameseCat));
