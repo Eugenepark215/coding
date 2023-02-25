f154292beaf64c4de3d3e59a97045aae86b4d905
Eugene Park <epark215@gmail.com>
2023-02-25 02:44:13 +0000

    Added prop to interface

--- advancedObjectTypes/deepTypes/index.ts
+++ advancedObjectTypes/deepTypes/index.ts
@@ -1,26 +1,36 @@
-// interface Directory {
-//   addFile: (name: string) => void;
-//   // Define a config type member here
+interface Directory {
+  addFile: (name: string) => void;
+  // Define a config type member here
+  config: {
+    default: {
+      encoding:string,
+      permissions:string,
+    }
+  }
+}
 
-// }
+class DesktopDirectory implements Directory {
+  config = {
+    default: {
+      encoding: 'utf-8',
+      permissions: 'drw-rw-rw-'
+    }
+  };
 
-// class DesktopDirectory implements Directory {
-//   config = {
-//     default: {
-//       encoding: 'utf-8',
-//       permissions: 'drw-rw-rw-'
-//     }
-//   };
+  addFile(name: string) {
+    // eslint-disable-next-line no-console
+    console.log(`Adding file: ${name}`);
+  }
 
-//   addFile(name: string) {
-//     console.log(`Adding file: ${name}`);
-//   }
+  showPreview(name: string) {
+    // eslint-disable-next-line no-console
+    console.log(`Opening preview of file: ${name}`);
+  }
+}
 
-//   showPreview(name: string) {
-//     console.log(`Opening preview of file: ${name}`);
-//   }
-// }
+const Desktop = new DesktopDirectory();
 
-// const Desktop = new DesktopDirectory();
+// eslint-disable-next-line no-console
+console.log(Desktop.config);
 
-// console.log(Desktop.config);
+export {};
