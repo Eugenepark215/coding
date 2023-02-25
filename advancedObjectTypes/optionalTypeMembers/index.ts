6e6f7e7ee3c6e3d4ab109c536346a38eadf9b5c9
Eugene Park <epark215@gmail.com>
2023-02-25 04:36:45 +0000

    initial commit optionalTypeMembers

---
+++ advancedObjectTypes/optionalTypeMembers/index.ts
@@ -0,0 +1,20 @@
+// // Write an interface here
+
+// function getUserName(options) {
+//   if (options.firstName && options.lastName) {
+//     return console.log(`${options.firstName} ${options.lastName}`);
+//   }
+
+//   return console.log(options.username);
+// }
+
+// getUserName({
+//   firstName: 'Mr.',
+//   lastName: 'Oshiro',
+//   username: 'hotelowner304'
+// });
+
+// getUserName({
+//   firstName: 'Madeline',
+//   username: 'mountainClimber'
+// });
