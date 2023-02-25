05cd568f0f13da2e11199c14e9ac73b460b065d4
Eugene Park <epark215@gmail.com>
2023-02-25 02:56:23 +0000

    Initial commit extendingInterfaces

---
+++ advancedObjectTypes/extendingInterfaces/index.ts
@@ -0,0 +1,13 @@
+// interface Developer {
+//   code: () => void;
+// }
+
+// // Add your interface here
+
+// const me: Developer = {
+//   code: () => console.log('Headphones on. Coffee brewed. Editor open.'),
+//   name: 'Corrina',
+//   hobbies: ['Building rockets']
+// };
+
+// me.code();
