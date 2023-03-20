d92a1e3ac8f8b81c4a26f133a3af94504301f22a
Eugene Park <epark215@gmail.com>
2023-03-20 05:49:19 +0000

    Refactored: removed 2 variables

--- findEmailDomain/index.js
+++ findEmailDomain/index.js
@@ -1,10 +1,4 @@
 function solution(address) {
-  var letters = address.split('');
-  var domainArray = letters.slice(address.lastIndexOf('@') + 1);
-  var domain = '';
-  for (var i = 0; i < domainArray.length; i++) {
-    domain += domainArray[i];
-  }
-  return domain;
+  return address.slice(address.lastIndexOf('@') + 1);
 }
 solution('"very.unusual.@.unusual.com"@usual.com');
--- findEmailDomain/index.ts
+++ findEmailDomain/index.ts
@@ -1,10 +1,4 @@
 function solution(address: string): string {
-  const letters: string[] = address.split('');
-  const domainArray: string[] = letters.slice(address.lastIndexOf('@') + 1);
-  let domain = '';
-  for (let i = 0; i < domainArray.length; i++) {
-    domain += domainArray[i];
-  }
-  return domain;
+  return address.slice(address.lastIndexOf('@') + 1);
 }
 solution('"very.unusual.@.unusual.com"@usual.com');
