9907387871bad8d5ed0363bcc5736d08f7073812
Eugene Park <epark215@gmail.com>
2023-04-08 01:14:33 +0000

    Comments

--- messageFromBinaryCode/index.js
+++ messageFromBinaryCode/index.js
@@ -2,8 +2,11 @@ function solution(code) {
   var eightLetterArray = [];
   var tempArray = [];
   for (var i = 0; i < code.length + 1; i++) {
+    // once tempArray has length of 8 (8bit)
     if (tempArray.length === 8) {
+      // push joined string value of tempArray into eightLetterArray
       eightLetterArray.push(tempArray.join(''));
+      // reset temoArray
       tempArray = [];
     }
     tempArray.push(code[i]);
@@ -11,6 +14,7 @@ function solution(code) {
   function revise(eightBit) {
     var ascii = '';
     for (var k = 0; k < eightBit.length; k++) {
+      // for each string value in array change to utf-16 into ascii
       ascii += String.fromCharCode(parseInt(eightBit[k], 2));
     }
     return ascii;
