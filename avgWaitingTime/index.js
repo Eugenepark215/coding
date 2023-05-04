eb6dfe8613d3ff7c75462be1f08b0b3a7ccbfb68
Eugene Park <epark215@gmail.com>
2023-05-04 05:51:59 +0000

    Refactored - deleted array and reduce function, stored sum in variable

--- avgWaitingTime/index.js
+++ avgWaitingTime/index.js
@@ -1,5 +1,5 @@
 function averageWaitingTime(customers) {
-  var avgTimeArray = [];
+  var sum = 0;
   var finishTime = customers[0][0];
   for (var i = 0; i < customers.length; i++) {
     var currentTime = customers[i][0];
@@ -8,10 +8,7 @@ function averageWaitingTime(customers) {
       finishTime = currentTime;
     }
     finishTime += waitTime;
-    avgTimeArray.push(finishTime - currentTime);
+    sum += finishTime - currentTime;
   }
-  var sum = avgTimeArray.reduce(function (a, b) {
-    return a + b;
-  });
-  return sum / avgTimeArray.length;
+  return sum / customers.length;
 }
--- avgWaitingTime/index.ts
+++ avgWaitingTime/index.ts
@@ -1,5 +1,5 @@
 function averageWaitingTime(customers: number[][]): number {
-  const avgTimeArray: number[] = [];
+  let sum = 0;
   let finishTime = customers[0][0];
   for (let i = 0; i < customers.length; i++) {
     const currentTime = customers[i][0];
@@ -8,11 +8,7 @@ function averageWaitingTime(customers: number[][]): number {
       finishTime = currentTime;
     }
     finishTime += waitTime;
-    avgTimeArray.push(finishTime - currentTime);
+    sum += finishTime - currentTime;
   }
-  const sum = avgTimeArray.reduce((a, b) => {
-    return a + b;
-  });
-
-  return sum / avgTimeArray.length;
+  return sum / customers.length;
 }
