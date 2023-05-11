87b1ebdab78133e32df83eb8fadaa858f1507763
Eugene Park <epark215@gmail.com>
2023-05-11 03:57:20 +0000

    time limit exceeded

---
+++ guessHighOrLow/index.ts
@@ -0,0 +1,28 @@
+/**
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return     -1 if num is higher than the picked number
+ *     1 if num is lower than the picked number
+ *               otherwise return 0
+ */
+function guess(num:number) :number {
+  // pick being arbitraty number called from api guess
+  // eslint-disable-next-line prefer-const
+  let pick = 0;
+  if (pick === num) return 0;
+  // eslint-disable-next-line
+  else if (pick > num) return 1;
+  else return -1;
+}
+
+function guessNumber(n: number): number {
+  let pick = n;
+  while (guess(pick) !== 0) {
+    if (guess(n) === -1) {
+      pick--;
+    } else if (guess(n) === 1) {
+      pick++;
+    }
+  }
+  return pick;
+}
