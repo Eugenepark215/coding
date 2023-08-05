1fea8d986be5d9a84d6aa65716971c4ca3c97fce
Eugene Park <epark215@gmail.com>
2023-08-05 00:24:30 +0000

    passes test

---
+++ rotateString/index.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ * split into array
+ * loop the length of string
+ * push && shift
+ */
+var rotateString = function (s, goal) {
+  const splitS = s.split('');
+  if (s === goal) {
+    return true;
+  }
+  for (let i = 0; i < s.length; i++) {
+    const current = splitS.shift();
+    splitS.push(current);
+    const joinedSplitS = splitS.join('');
+    if (joinedSplitS === goal) {
+      return true;
+    }
+  }
+  return false;
+};
+
+rotateString('abcde', 'bcdea');
