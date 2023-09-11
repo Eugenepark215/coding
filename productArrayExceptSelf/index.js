2298a6e045d98531df93f360f890977bb46532a0
Eugene Park <epark215@gmail.com>
2023-09-11 02:46:16 +0000

    completed

--- productArrayExceptSelf/index.js
+++ productArrayExceptSelf/index.js
@@ -1,25 +1,19 @@
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-  if (pack || arguments.length === 2) {
-    for (var i = 0, l = from.length, ar; i < l; i++) {
-      if (ar || !(i in from)) {
-        if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-        ar[i] = from[i];
-      }
-    }
-  }
-  return to.concat(ar || Array.prototype.slice.call(from));
-};
 function productExceptSelf(nums) {
-  var answerArray = [];
-  for (var i = 0; i < nums.length; i++) {
-    var numsArray = __spreadArray([], nums, true);
-    numsArray.splice(i, 1);
-    var inital = numsArray[0];
-    for (var j = 1; j < numsArray.length; j++) {
-      inital *= numsArray[j];
-    }
-    answerArray.push(inital);
+  // create new array with length of nums and fill with values of 1
+  var result = Array(nums.length).fill(1);
+  var numsLen = nums.length;
+  // creates new array with 2 elements value of 1
+  var curr = Array(2).fill(1);
+  for (var i = 1; i < numsLen; i++) {
+    // curr[0] is multiplied by each value of nums by index order on each loop
+    curr[0] *= nums[i - 1];
+    // curr[1] is multipled by each value of nums by reverse index order on each loop
+    curr[1] *= nums[numsLen - i];
+    // the value starting from index 1 is multipled by curr[0] for each loop
+    result[i] *= curr[0];
+    // the value starting from second to last index is multipled by curr[1] for each loop
+    result[numsLen - i - 1] *= curr[1];
   }
-  return answerArray;
+  return result;
 }
 productExceptSelf([1, 2, 3, 4]);
