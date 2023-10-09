2eea4dde2b4cb5f4c5ee69b06d7ea07cc6316e4d
Eugene Park <epark215@gmail.com>
2023-10-09 20:53:51 +0000

    pseudocode

---
+++ lexoSmallestPalindrome/index.js
@@ -0,0 +1,9 @@
+// function makeSmallestPalindrome(s: string): string {
+// }
+// make variable array that contains alaphabet
+// split the string s into 2 variables
+// if the length of the string is uneven eg 5 (s.length % 2 !== 0)
+// then Math.floor();
+// compare the letters of string1[i] to string2[i]
+// if there is a discrepancy find index of the letter in regards const alpahbet and replace splice
+// create variable that will be placeholder for string
