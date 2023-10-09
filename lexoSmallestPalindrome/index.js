5478546ccfa6d566774ad338bc26b551913cf460
Eugene Park <epark215@gmail.com>
2023-10-09 21:33:46 +0000

    refactored code less lines

--- lexoSmallestPalindrome/index.js
+++ lexoSmallestPalindrome/index.js
@@ -1,39 +1,20 @@
 function makeSmallestPalindrome(s) {
-  var sArray = s.split('');
-  var newString = [];
-  var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  var string1 = [];
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  var string2 = [];
-  // split and fill string1 and string2
-  for (var i = 0; i < Math.floor(s.length / 2); i++) {
-    string1.push(sArray.shift());
-    string2.push(sArray.pop());
-  }
-  // if length is uneven push middle string element into newString
-  if (s.length % 2 !== 0) {
-    newString.push(s[Math.floor(s.length / 2)]);
-  }
-  // check values and splice in accordance to alphabet
-  for (var j = 0; j < string1.length; j++) {
-    if (string1[j] !== string2[j]) {
-      if (alphabet.indexOf(string1[j]) > alphabet.indexOf(string2[j])) {
-        string1.splice(j, 1, string2[j]);
+  // create array from string;
+  var arr = Array.from(s);
+  var n = arr.length;
+  for (var i = 0; i < n / 2; i++) {
+    var left = arr[i];
+    var right = arr[n - i - 1];
+    if (left !== right) {
+      // can compare string lexographically
+      if (left < right) {
+        // replace string given condition
+        arr[n - i - 1] = left;
       } else {
-        string2.splice(j, 1, string1[j]);
+        arr[i] = right;
       }
     }
   }
-  newString.unshift.apply(newString, string1);
-  newString.push.apply(newString, string2.reverse());
-  return newString.join('');
+  return arr.join('');
 }
 makeSmallestPalindrome('abcd');
-// make variable array that contains alaphabet
-// split the string s into 2 variables
-// if the length of the string is uneven eg 5 (s.length % 2 !== 0)
-// then Math.floor();
-// compare the letters of string1[i] to string2[i]
-// if there is a discrepancy find index of the letter in regards const alpahbet and replace splice
-// create variable that will be placeholder for string
--- lexoSmallestPalindrome/index.ts
+++ lexoSmallestPalindrome/index.ts
@@ -1,40 +1,24 @@
 function makeSmallestPalindrome(s: string): string {
-  const sArray: string[] = s.split('');
-  const newString: string[] = [];
-  const alphabet:string[] = 'abcdefghijklmnopqrstuvwxyz'.split('');
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const string1: any = [];
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const string2: any = [];
-  // split and fill string1 and string2
-  for (let i = 0; i < Math.floor(s.length / 2); i++) {
-    string1.push(sArray.shift());
-    string2.push(sArray.pop());
-  }
-  // if length is uneven push middle string element into newString
-  if (s.length % 2 !== 0) {
-    newString.push(s[Math.floor(s.length / 2)]);
-  }
-  // check values and splice in accordance to alphabet
-  for (let j = 0; j < string1.length; j++) {
-    if (string1[j] !== string2[j]) {
-      if (alphabet.indexOf(string1[j]) > alphabet.indexOf(string2[j])) {
-        string1.splice(j, 1, string2[j]);
+  // create array from string;
+  const arr = Array.from(s);
+  const n = arr.length;
+
+  for (let i = 0; i < n / 2; i++) {
+    const left = arr[i];
+    const right = arr[n - i - 1];
+
+    if (left !== right) {
+      // can compare string lexographically
+      if (left < right) {
+        // replace string given condition
+        arr[n - i - 1] = left;
       } else {
-        string2.splice(j, 1, string1[j]);
+        arr[i] = right;
       }
     }
   }
-  newString.unshift(...string1);
-  newString.push(...string2.reverse());
-  return newString.join('');
+
+  return arr.join('');
 }
 
 makeSmallestPalindrome('abcd');
-// make variable array that contains alaphabet
-// split the string s into 2 variables
-// if the length of the string is uneven eg 5 (s.length % 2 !== 0)
-// then Math.floor();
-// compare the letters of string1[i] to string2[i]
-// if there is a discrepancy find index of the letter in regards const alpahbet and replace splice
-// create variable that will be placeholder for string
