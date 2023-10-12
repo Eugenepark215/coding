af4c15625e8327c2c94c5668a6e1289ad8d5b69a
Eugene Park <epark215@gmail.com>
2023-10-12 00:07:39 +0000

    pseudocode

---
+++ deleteCharactersToMakeFancyString/index.ts
@@ -0,0 +1,9 @@
+// given string s return a new string that doesnt contain 3 same consecutive letters
+// use for loop to iterate through string s
+// create variable that will count the number of occurences
+// create variable that will reference the letter being referenced
+// create variable arrat that will hold values of s in respect to condition
+// conditional to check if current is equal to string[index]
+// nested conditional that will check the value of counter that will add to counter and push value into Array
+// else condtional that will set counter to 1 and set reference vairable to s[index]
+// rerun new array
