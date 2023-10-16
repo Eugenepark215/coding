687dbe1ebceb6db34e9267dead8f9898a402682f
Eugene Park <epark215@gmail.com>
2023-10-16 20:04:07 +0000

    pseudocode

---
+++ pascalsTriangle2/index.ts
@@ -0,0 +1,4 @@
+// given an integer,n, return an array that contains the elemennts:number of the nth row of a pascals triangle
+// loop that traverses down pascals triangle
+// create variable that will be placeholder to hold values of triangle
+// once the final loop occurs place values of arary
