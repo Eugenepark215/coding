f163f178a3880dde96b2195707b5c039247cc55e
Eugene Park <epark215@gmail.com>
2023-11-09 04:04:33 +0000

    pseudo

---
+++ minCostClimbingStairs/index.js
@@ -0,0 +1,6 @@
+// can start at index 0 or 1
+// can increase index by 1 or 2
+// finishing is exceeding length of array
+// have to compare which cost is cheaper
+// have to iterate through array
+// have to compare if either index + 1 or index + 2 is cheaper
