257da1790e2dd7c6ffb57e6986061b6b51a2cb24
Eugene Park <epark215@gmail.com>
2023-11-10 21:12:09 +0000

    pseudo

---
+++ percentageOfLetterInString/index.ts
@@ -0,0 +1,7 @@
+// input: 2 strings, first string of multiple length, second length = 1
+// return percentage number eg 33 for 1/3
+// iterate through string
+// use map to set key value(occurences of specific s) pairs
+// map.get(s) to find occurence
+// divide value of map.get(s) to length of given string
+// if s is not within the given string return 0
