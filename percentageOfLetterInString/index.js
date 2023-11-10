1f0fcc5e9f9a0bc357299982b89f74318883f521
Eugene Park <epark215@gmail.com>
2023-11-10 21:19:20 +0000

    passes test

--- percentageOfLetterInString/index.js
+++ percentageOfLetterInString/index.js
@@ -5,3 +5,18 @@
 // map.get(s) to find occurence
 // divide value of map.get(s) to length of given string
 // if s is not within the given string return 0
+function percentageLetter(s, letter) {
+  if (!s.includes(letter)) {
+    return 0;
+  } else {
+    var newMap = new Map();
+    for (var i = 0; i < s.length; i++) {
+      if (!newMap.get(s[i])) {
+        newMap.set(s[i], 1);
+      } else {
+        newMap.set(s[i], newMap.get(s[i]) + 1);
+      }
+    }
+    return Math.floor((newMap.get(letter) / s.length) * 100);
+  }
+}
