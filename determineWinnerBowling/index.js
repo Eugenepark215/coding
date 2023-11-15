71b1316f2bbca590eca5e8b0d1ce5d289580273d
Eugene Park <epark215@gmail.com>
2023-11-15 23:57:02 +0000

    pseduo

---
+++ determineWinnerBowling/index.js
@@ -0,0 +1,8 @@
+// 2 array arguements
+// return 1, 2, 0
+// return sum of array and return which array has larger sum 0 is draw
+// if integer within array is 10 then for the values of the next 2 elements will be doubled
+// iterate through for loop
+// use variables to confirm condirionals
+// if array[i] === 10 set conditional to true
+// set counter variable
