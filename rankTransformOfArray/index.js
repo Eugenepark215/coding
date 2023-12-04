b3ee4c911585504fcecb171da1997cda5944ad7b
Eugene Park <epark215@gmail.com>
2023-12-04 19:49:32 +0000

    passes all test except time limit exceeded

--- rankTransformOfArray/index.js
+++ rankTransformOfArray/index.js
@@ -1,2 +1,20 @@
-// given array replace the elements with rank of value
-// duplicate numbers must have same rank
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+  if (pack || arguments.length === 2) {
+    for (var i = 0, l = from.length, ar; i < l; i++) {
+      if (ar || !(i in from)) {
+        if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+        ar[i] = from[i];
+      }
+    }
+  }
+  return to.concat(ar || Array.prototype.slice.call(from));
+};
+function arrayRankTransform(arr) {
+  var shallow = arr.slice();
+  var newSet = __spreadArray([], new Set(shallow), true);
+  var sortedSet = newSet.sort(function (a, b) { return a - b; });
+  for (var i = 0; i < arr.length; i++) {
+    arr.splice(i, 1, (sortedSet.indexOf(arr[i]) + 1));
+  }
+  return arr;
+}
--- rankTransformOfArray/index.ts
+++ rankTransformOfArray/index.ts
@@ -1,2 +1,9 @@
-// given array replace the elements with rank of value
-// duplicate numbers must have same rank
+function arrayRankTransform(arr: number[]): number[] {
+  const shallow = arr.slice();
+  const newSet = [...new Set(shallow)];
+  const sortedSet = newSet.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    arr.splice(i, 1, (sortedSet.indexOf(arr[i]) + 1));
+  }
+  return arr;
+}
