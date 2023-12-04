71f5db8b533a5a7167d33bcf4ad45bb106a9416b
Eugene Park <epark215@gmail.com>
2023-12-04 17:57:51 +0000

    passes test

--- largest3SameDigitNumber/index.js
+++ largest3SameDigitNumber/index.js
@@ -1,3 +1,11 @@
-// return the largest value substring of length 3 that consists of same digits
-// store substring in arrat
-// return Math.max(...array)
+function largestGoodInteger(num) {
+  var array1 = [];
+  for (var i = 0; i < num.length; i++) {
+    if (num[i + 2] === num[i + 1] && num[i + 2] === num[i]) {
+      array1.push(parseInt(num.slice(i, i + 3)));
+    }
+  }
+  if (array1.length === 0) { return ''; }
+  if (Math.max.apply(Math, array1) === 0) { return '000'; }
+  return Math.max.apply(Math, array1).toString();
+}
--- largest3SameDigitNumber/index.ts
+++ largest3SameDigitNumber/index.ts
@@ -1,3 +1,11 @@
-// return the largest value substring of length 3 that consists of same digits
-// store substring in arrat
-// return Math.max(...array)
+function largestGoodInteger(num: string): string {
+  const array1:number[] = [];
+  for (let i = 0; i < num.length; i++) {
+    if (num[i + 2] === num[i + 1] && num[i + 2] === num[i]) {
+      array1.push(parseInt(num.slice(i, i + 3)));
+    }
+  }
+  if (array1.length === 0) return '';
+  if (Math.max(...array1) === 0) return '000';
+  return Math.max(...array1).toString();
+}
