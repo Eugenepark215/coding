de4f56bab1e8589d30d5a79426cae7c5a2b0e14e
Eugene Park <epark215@gmail.com>
2023-12-10 01:21:50 +0000

    compelted

--- transposeMatrix/index.ts
+++ transposeMatrix/index.ts
@@ -2,3 +2,16 @@
 // return array so that iniital array is flipped
 // [[1,2,3],[4,5,6],[7,8,9]] -> [[1,4,7],[2,5,8],[3,6,9]]
 // take iniital  index of arrays push into new array and return
+
+function transpose(matrix: number[][]): number[][] {
+  const flipped: number[][] = [];
+  let arrayInsideArray: number[] = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      arrayInsideArray.push(matrix[j][i]);
+    }
+    flipped.push(arrayInsideArray);
+    arrayInsideArray = [];
+  }
+  return flipped;
+}
