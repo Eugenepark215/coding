58c83bc65d44269041c9ff9cded30ce74a1a284e
Eugene Park <epark215@gmail.com>
2023-12-10 01:18:58 +0000

    pseudo

---
+++ transposeMatrix/index.js
@@ -0,0 +1,4 @@
+// return array that contains array
+// return array so that iniital array is flipped
+// [[1,2,3],[4,5,6],[7,8,9]] -> [[1,4,7],[2,5,8],[3,6,9]]
+// take iniital  index of arrays push into new array and return
