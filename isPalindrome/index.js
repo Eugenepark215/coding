2422d6e06dffa067b7b97db44f779a5721ee3da5
Eugene Park <epark215@gmail.com>
2024-01-26 18:27:47 +0000

    pseudo

---
+++ isPalindrome/index.js
@@ -0,0 +1,5 @@
+// given string; removing any non alphanumeric values check to see if palindrome
+// return boolean
+// iterate through given string via for loop
+// utilize variable to hold alphanumeric values
+// split, reverse, join then compare value
