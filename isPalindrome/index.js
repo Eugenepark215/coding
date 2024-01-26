dcb453bb284730e207afbdb92dddaeff5b896375
Eugene Park <epark215@gmail.com>
2024-01-26 18:41:25 +0000

    refactored - reversed string within intial loop

--- isPalindrome/index.js
+++ isPalindrome/index.js
@@ -1,21 +1,11 @@
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-  if (pack || arguments.length === 2) {
-    for (var i = 0, l = from.length, ar; i < l; i++) {
-      if (ar || !(i in from)) {
-        if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-        ar[i] = from[i];
-      }
-    }
-  }
-  return to.concat(ar || Array.prototype.slice.call(from));
-};
 function isPalindrome(s) {
   var stringsOnly = '';
+  var reverse = '';
   for (var i = 0; i < s.length; i++) {
     if (/^[A-Za-z0-9]+$/.test(s[i])) {
       stringsOnly += s[i].toLowerCase();
+      reverse = s[i].toLowerCase() + reverse;
     }
   }
-  var splitStringArray = __spreadArray([], stringsOnly, true).reverse().join('');
-  return splitStringArray === stringsOnly;
+  return reverse === stringsOnly;
 }
